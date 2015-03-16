13bc8799069964b74024f1d2e2889e46cd8509d9
Matthew Vincent <matt@averails.com>
2015-03-16 22:01:28 +0100

    precompiling images js

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,4 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( progressbar.gif )
 Rails.application.config.assets.precompile += %w( loading.gif )
+Rails.application.config.assets.precompile += %w( images.js )
