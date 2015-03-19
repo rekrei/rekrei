a1b80aefaf1d2de4aea87d45eb07c941758cbf22
Matthew Vincent <matt@averails.com>
2015-03-19 22:10:58 +0100

    fixing nill images for artefacts

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,6 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( progressbar.gif )
-Rails.application.config.assets.precompile += %w( loading.gif page-background.png stay-connected.png )
+Rails.application.config.assets.precompile += %w( loading.gif )
+Rails.application.config.assets.precompile += %w( page-background.png )
+Rails.application.config.assets.precompile += %w( stay-connected.png )
 Rails.application.config.assets.precompile += %w( images.js )
