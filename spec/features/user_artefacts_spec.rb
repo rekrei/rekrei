83450c07c0eaacc59c3bd8a09bd919d87548ace9
Matthew Vincent <matt@averails.com>
2015-03-23 08:48:47 +0100

    updating spec to fit new design

--- spec/features/user_artefacts_spec.rb
+++ spec/features/user_artefacts_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 describe "artefacts", :type=>:feature do
   scenario "guest attempts to create an artefact" do
     visit artefacts_path
-    expect(page).to have_content "You must be signed in to create an artefact"
+    expect(page).to have_content "Login to create an artrefact"
   end
 end
