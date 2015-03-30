9293c50bb5416eb94c89d8a8b76cadf12b252c81
Matthew Vincent <matt@averails.com>
2015-03-30 18:21:06 +0200

    adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Projectmosul::Application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :artefacts do
     resources :assets
   end
@@ -12,16 +14,17 @@ Projectmosul::Application.routes.draw do
   root "pages#home"
   get "home", to: "pages#home", as: "home"
   get "gallery", to: "pages#gallery", as: "gallery"
+  get 'press', to: 'pages#press', as: "press"
   get "/contact", to: "pages#contact", as: "contact"
   post "/emailconfirmation", to: "pages#email", as: "email_confirmation"
   
   
   devise_for :users
 
-  namespace :admin do
-    root "base#index"
-    resources :users
+  # namespace :admin do
+  #   root "base#index"
+  #   resources :users
     
-  end
+  # end
 
 end
