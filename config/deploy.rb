e5565b6c397da89b23311d316ff887d50c01359a
Matthew Vincent <matt@averails.com>
2015-04-08 15:01:11 +0200

    unlinking bin dir, now versioned

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :repo_url, 'git@github.com:neshmi/projectmosul.git'
 set :linked_files, fetch(:linked_files, []).push('.env')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
