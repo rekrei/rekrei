3d7f1174520b62da2016f2192fa34f4d1a55e514
Matthew Vincent <matt@averails.com>
2015-04-15 16:21:54 +0200

    adding more to images show

--- app/views/api/v1/images/show.json.jbuilder
+++ app/views/api/v1/images/show.json.jbuilder
@@ -1,3 +1,10 @@
 json.(@image, :created_at, :updated_at)
 json.id @image.id
 json.url Rails.application.config_for(:project_mosul)['host'] + @image.image.url(:original)
+
+json.artefact do
+  json.id @image.try(:artefact).try(:id)
+  json.uuid @image.try(:artefact).try(:uuid)
+  json.name @image.try(:artefact).try(:name)
+  json.uri artefact_url(@image.try(:artefact).try(:id))
+end
--- spec/controllers/api/v1/images_controller_spec.rb
+++ spec/controllers/api/v1/images_controller_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 describe Api::V1::ImagesController do
   before(:each) { request.headers['Accept'] = "application/vnd.marketplace.v1" }
 
-  describe 'get #index' do
+  describe 'get /images' do
     def get_images
-      @image = FactoryGirl.create_list :image, 3
+      FactoryGirl.create_list :image, 3
       get :index, format: :json
     end
 
@@ -18,6 +18,30 @@ describe Api::V1::ImagesController do
     it 'should respond with 200' do
       get_images
       expect(response).to be_success
+    end
+  end
+
+  describe 'get images/:id' do
+    render_views
+    def get_image
+      @image = FactoryGirl.create :image
+      get :show, id: @image.id, format: :json
+      @image_response = JSON.parse(response.body, symbolize_names: true)
+    end
+
+    it "should respond with 200" do
+      get_image
+      expect(response).to be_success
+    end
+
+    it "should assign id" do
+      get_image
+      expect(@image_response[:id]).to eq @image.id
+    end
+
+    it 'should assign url' do
+      get_image
+      expect(@image_response[:url]).to contain "http://test.host/system/images/images/000/000/#{@image.id}/original/test1500white.png?"
     end
   end
 end
