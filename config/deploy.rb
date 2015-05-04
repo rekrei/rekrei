21ee75b23885e7d3ee07290e8509ffcb0e4cbebc
Matthew Vincent <matt@averails.com>
2015-05-04 12:16:35 +0200

    updating capistrano recipes

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,35 +5,12 @@ lock '3.3.5'
 
 set :application, 'projectmosul'
 set :repo_url, 'git@github.com:neshmi/projectmosul.git'
-set :passenger_restart_with_sudo, true
-
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
-
-# Default deploy_to directory is /var/www/my_app_name
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
+set :passenger_restart_command, 'touch'
+set :passenger_restart_options, -> { "#{deploy_to}/current/tmp/restart.txt" }
 set :log_level, :debug
 
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
 set :linked_files, fetch(:linked_files, []).push('.env')
-
-# Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
 set :keep_releases, 5
 
 after 'deploy:finished', 'airbrake:deploy'
@@ -41,10 +18,6 @@ after 'deploy:finished', 'airbrake:deploy'
 namespace :deploy do
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
     end
   end
 end
