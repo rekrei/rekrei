9ea59cadce71565d42460fa72580a0fefb5bd0ee
Matthew Vincent <matt@averails.com>
2015-05-24 13:30:56 +0200

    sidekiq update; database url fix

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,14 +1,7 @@
+Sidekiq.configure_server do |config|
+  config.redis = { url: 'redis://localhost:6379/0', namespace: "projectmosul_#{Rails.env}" }
+end
+
 Sidekiq.configure_client do |config|
   config.redis = { url: 'redis://localhost:6379/0', namespace: "projectmosul_#{Rails.env}" }
 end  
-
-
-Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0', namespace: "projectmosul_#{Rails.env}" }
-
-  database_url = ENV['DATABASE_URL']
-  if database_url
-    ENV['DATABASE_URL'] = "#{database_url}?pool=25"
-    ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
-  end
-end
