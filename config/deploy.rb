31bbe79f1ff297c9766d574cfc9df749dd91e3d0
Matthew Vincent <matt@averails.com>
2015-05-26 21:52:13 +0200

    updating deploy recipies

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,5 @@
 require 'airbrake/capistrano3'
+require 'byebug'
 
 # config valid only for current version of Capistrano
 lock '3.3.5'
@@ -13,6 +14,8 @@ set :linked_files, fetch(:linked_files, []).push('.env')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 set :keep_releases, 5
 
+# set :sidekiq_worker, -> { fetch(:sidekiq_worker) }
+
 after 'deploy:finished', 'airbrake:deploy'
 
 namespace :deploy do
@@ -23,31 +26,59 @@ namespace :deploy do
 end
 
 namespace :sidekiq do
+  def sidekiq_pid
+    File.join(shared_path,'tmp','pids','sidekiq.pid')
+  end
+
+  def pid_file_exists?
+    test(*("[ -f #{sidekiq_pid} ]").split(' '))
+  end
+
+  def pid_process_exists?
+    pid_file_exists? and test(*("kill -0 $( cat #{sidekiq_pid} )").split(' '))
+  end
+
+  def sidekiqctl_cmd 
+    "bundle exec sidekiqctl"
+  end
+
+  def sidekiq_worker
+    fetch :sidekiq_worker
+  end
+
   desc 'Start the sidekiq workers via Upstart'
   task :start do
-    sudo 'start staging-worker-1'
+    on roles(:app) do
+      if !pid_process_exists?
+        execute :sudo, "/sbin/start #{sidekiq_worker}"
+      end
+    end
   end
- 
+
   desc 'Stop the sidekiq workers via Upstart'
   task :stop do
-    sudo 'stop staging-worker-1 || true'
+    on roles(:app) do
+      if pid_process_exists?
+        execute :sudo, "/sbin/stop #{sidekiq_worker}"
+        execute "rm #{sidekiq_pid}"
+      end
+    end
   end
- 
+
   desc 'Restart the sidekiq workers via Upstart'
   task :restart do
-    sudo 'stop staging-worker-1 || true'
-    sudo 'start staging-worker-1'
+    on roles(:app) do
+      invoke "sidekiq:stop"
+      invoke "sidekiq:start"
+    end
   end
- 
+
   desc "Quiet sidekiq (stop accepting new work)"
   task :quiet do
-    pid_file       = "#{current_path}/tmp/pids/sidekiq.pid"
     sidekiqctl_cmd = "bundle exec sidekiqctl"
-    run "if [ -d #{current_path} ] && [ -f #{pid_file} ] && kill -0 `cat #{pid_file}`> /dev/null 2>&1; then cd #{current_path} && #{sidekiqctl_cmd} quiet #{pid_file} ; else echo 'Sidekiq is not running'; fi"
+    run "if [ -d #{current_path} ] && [ -f #{sidekiq_pid} ] && kill -0 `cat #{sidekiq_pid}`> /dev/null 2>&1; then cd #{current_path} && #{sidekiqctl_cmd} quiet #{sidekiq_pid} ; else echo 'Sidekiq is not running'; fi"
   end
 end
  
-before 'deploy:update_code', 'sidekiq:quiet'
-after  'deploy:stop',        'sidekiq:stop'
-after  'deploy:start',       'sidekiq:start'
-before 'deploy:restart',     'sidekiq:restart'
+before 'deploy:updating', 'sidekiq:quiet'
+before 'deploy:restart',  'sidekiq:restart'
