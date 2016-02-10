0f8de12412e1a45d1a05aed846fe45f7bd706895
Matthew Vincent <matt@averails.com>
2016-02-10 11:47:30 +0100

    airbrake 5 configuration

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,3 +1,8 @@
-Airbrake.configure do |config|
-  config.api_key = '700bdd04b30f2bd81507183a05998e35'
+Airbrake.configure do |c|
+  c.project_id = ENV['AIRBRAKE_PROJECT_ID'] || ""
+  c.project_key = ENV['AIRBRAKE_API_KEY'] || ""
+  c.root_directory = Rails.root
+  c.logger = Rails.logger
+  c.environment = Rails.env
+  c.ignore_environments = %w(test development)
 end
