2d520fb91cf22aba2ffdcdad15012edd11dd21b0
Matthew Vincent <matt@averails.com>
2016-02-21 12:52:49 +0100

    puma updates

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,4 @@
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['MAX_THREADS'] || 5)
-threads threads_count, threads_count
+threads 1, Integer(ENV['MAX_THREADS'] || 6)
 
 preload_app!
 
@@ -8,9 +6,9 @@ rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
+
 on_worker_boot do
-    # Worker specific setup for Rails 4.1+
-  #   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  #     ActiveRecord::Base.establish_connection
-  #     end
+  require "active_record"
+  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
 end
