afde4fdc8c9d4299b9176547c1b92a00edee6331
Matthew Vincent <matt@averails.com>
2016-02-21 13:19:13 +0100

    changing more PM to Rekrei references

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 # General application helpers here
 module ApplicationHelper
   def title(value)
-    @title = "#{value} | Project Mosul" unless value.nil?
+    @title = "#{value} | Rekrei" unless value.nil?
   end
 end
