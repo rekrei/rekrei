56d97b02a559f0cf22cb0cc1cc4bc2ca1f3b8312
Matthew Vincent <matt@averails.com>
2016-05-11 20:39:14 +0200

    adding single cov to spec files

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+SingleCov.covered!
 
 describe Image do
   it { should have_many(:parent_matches) }
--- spec/models/reconstruction_spec.rb
+++ spec/models/reconstruction_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+SingleCov.covered!
 
 describe Reconstruction do
   it { should respond_to(:name) }
