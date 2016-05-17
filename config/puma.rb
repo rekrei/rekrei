3a9d8b37ca5554d46da55c573372a5ababef8691
Matthew Vincent <matt@averails.com>
2016-05-17 13:18:08 +0200

    puma tweaks

--- config/puma.rb
+++ config/puma.rb
@@ -1,14 +1,15 @@
-threads 1, Integer(ENV['MAX_THREADS'] || 6)
+workers = Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
 
 preload_app!
 
 rackup      DefaultRackup
-port        3000
+port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
-
 
 on_worker_boot do
   require "active_record"
   ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+  ActiveRecord::Base.establish_connection(YAML.load_file("#{Rails.root}/config/database.yml")[rails_env])
 end
