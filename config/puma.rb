29a6d258b26c144bd64e9d4b1b7297b5004e949e
Matthew Vincent <matt@averails.com>
2016-05-19 16:11:00 +0200

    adding puma configuration tweaks for smaller nodes

--- config/puma.rb
+++ config/puma.rb
@@ -13,3 +13,13 @@ on_worker_boot do
   ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
   ActiveRecord::Base.establish_connection(YAML.load_file("#{Rails.root}/config/database.yml")[rails_env])
 end
+
+before_fork do
+  PumaWorkerKiller.config do |config|
+    config.ram           = 512 # mb
+    config.frequency     = 5    # seconds
+    config.percent_usage = 0.98
+    config.rolling_restart_frequency = 12 * 3600 # 12 hours in seconds
+  end
+  PumaWorkerKiller.start
+end
