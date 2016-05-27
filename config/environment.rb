973f8cd45626a44031d37c1c8dee96c2a8f7513f
Matthew Vincent <matt@averails.com>
2016-05-27 20:29:18 +0200

    adding mini_racer

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Projectmosul::Application.initialize!
 
-if !Rails.env.development
+if !Rails.env.development?
   Rails.logger = Le.new('a66c30f9-b5fc-4560-b62e-06e7fee0c660')
 else
   Rails.logger = Le.new('a66c30f9-b5fc-4560-b62e-06e7fee0c660', :debug => true, :local => true)
