17cf0eb1552731ac0c81e8b7deed1a3ca8ba7e62
Matthew Vincent <matt@averails.com>
2016-06-01 00:25:09 +0200

    prdocution logs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,4 @@ Projectmosul::Application.configure do
     password: ENV['SMTP_PWD']
   }
 end
-Rails.logger = Le.new('a66c30f9-b5fc-4560-b62e-06e7fee0c660')
+Rails.logger = Le.new('a66c30f9-b5fc-4560-b62e-06e7fee0c660') unless ENV['DEBUG'] == true
